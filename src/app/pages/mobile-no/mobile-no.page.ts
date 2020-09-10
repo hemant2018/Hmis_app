2f6bf60082d678d7ad7f9bdbe6f9c48dc1f8c4db
Hemantchauhan <hemantr@athmin.com>
2020-09-10 20:15:06 +0530

    validation of mobile no

--- src/app/pages/mobile-no/mobile-no.page.ts
+++ src/app/pages/mobile-no/mobile-no.page.ts
@@ -7,14 +7,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./mobile-no.page.css'],
 })
 export class MobileNoPage implements OnInit {
-
+  mobileNo:any;
+  mobileNOError:boolean = true;
+  inValidMobileNo:boolean = false;
   constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  mobileNoMethod(){
+    if(this.mobileNo != undefined){
+   if(this.mobileNo.toString().length >0){
+        this.inValidMobileNo = false;
+    } else{
+      this.inValidMobileNo = true;
+    }
+  }else{
+    this.inValidMobileNo= false;
+  }
+}
+
   navigate(){
-    this.router.navigate(['/sign-in/otpVerify'])
+    if(this.mobileNo != undefined){
+      this.inValidMobileNo = false;
+      console.log(this.mobileNo);
+        if(this.mobileNo.toString().length > 10){
+          this.mobileNOError = false
+          console.log('greater than 10')
+        }else if(this.mobileNo.toString().length === 10){
+          this.mobileNOError = true
+          this.router.navigate(['/sign-in/otpVerify'])
+        }else{
+          console.log('less than 10');
+          this.mobileNOError = false;
+        }
+    }else{
+        this.inValidMobileNo = true;
+        this.mobileNOError = true;
+    }
   }
 
 }
