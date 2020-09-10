38ee2a5d4cd2c1004e8a4d86f8645961e19debd3
le@levitatingelephant.com <worldwebarts@gmail.com>
2020-09-10 17:19:58 +0530

    traige added

--- src/app/triage/triage.component.ts
+++ src/app/triage/triage.component.ts
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TriageComponent implements OnInit {
 
+  
+
   constructor() { }
 
-  ngOnInit() {}
+   
+
+  ngOnInit() {   }
 
 }
