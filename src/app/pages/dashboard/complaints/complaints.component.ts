7e28964910efd01f5d3c492b490d1568d7852c81
Hemantchauhan <hemantr@athmin.com>
2020-09-16 10:19:50 +0530

    complete the complaints issue

--- src/app/pages/dashboard/complaints/complaints.component.ts
+++ src/app/pages/dashboard/complaints/complaints.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuController } from '@ionic/angular';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import {map, startWith, filter} from 'rxjs/operators';
-import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-complaints',
@@ -50,8 +47,37 @@ complaintsFormsDefinition = {
       }
 
 }
+painBtnType =[
+  {
+      "type":'BLUNT',
+      "value":0
+  },
+  {
+    "type":'SHARP',
+    "value":1
+  },
+  {
+    "type":'PIERCING',
+    "value":2
+  },
+  {
+    "type":'ACUTE',
+    "value":3
+  },
+  {
+    "type":'CHRONIC',
+    "value":4
+  },
+  {
+    "type":'SUDDEN',
+    "value":5
+  },
+  {
+    "type":'PERSISTENT',
+    "value":6
+  }
+]
 complaintForms = [];
-// symptomsForm= new FormControl();
 symptomsList = [];
   constructor(private menu: MenuController, private route:Router) { }
 
@@ -60,16 +86,8 @@ symptomsList = [];
     if(localStorage.getItem('complaintForm')){
       this.complaintForms = JSON.parse(localStorage.getItem('complaintForm'))
     }
-  //   this.symptomsList = this.symptomsForm.valueChanges.pipe(
-  //     startWith(''),
-  //     map(option => option ? this.symptomsfilter(option) : this.symptomValue.slice())
-  // );
   }
 
-  // symptomsfilter(value : string){
-  //   const filteredValue = value.toLowerCase();
-  //   return this.symptomValue.filter(option => option.symptoms.toLowerCase().indexOf(filteredValue.toLowerCase()) === 0);
-  // }
 
   filterSymptoms(value){
       console.log(value)
@@ -113,15 +131,25 @@ symptomsList = [];
     const definitionObj = JSON.parse(JSON.stringify(this.complaintsFormsDefinition[type]));
     this.complaintForms.unshift(definitionObj);
   }
-    
+   
+       //  to maintain the active state of button
+       var that = this
+       setTimeout(function(){
+         that.maintainActiveStateBtn();
+       }, 500)
 
 }
 
 
 removeComplaintFrom(i){
   this.complaintForms.splice(i, 1);
+  var that = this
+  setTimeout(function(){
+    that.maintainActiveStateBtn();
+  }, 500)
   localStorage.setItem('complaintForm', JSON.stringify(this.complaintForms))
-  if(this.complaintForms.length === 0){
+      
+    if(this.complaintForms.length === 0){
     localStorage.removeItem('complaintForm');
   }
 }
@@ -129,6 +157,92 @@ removeComplaintFrom(i){
 formSubmit(){
   localStorage.setItem('complaintForm', JSON.stringify(this.complaintForms))
   alert('form submitted');
+}
+
+// button active & in acitve
+maintainActiveStateBtn(){
+  this.complaintForms.forEach((obj,index) =>{
+    if(obj.pain.length != 0 && obj.type === 'pain'){
+          obj.pain.forEach(value =>{
+              this.painBtnType.forEach(val =>{
+                  if(val.type === value){
+                      document.getElementsByClassName('pain-'+ index)[val.value].classList.add('active');
+                  }
+               })
+          })
+
+    } else if(obj.pain.length != 0  && obj.type === 'polyuria'){
+      obj.pain.forEach(value =>{
+          this.painBtnType.forEach(val =>{
+            if(val.type === value){
+                  document.getElementsByClassName('plyria-'+ index)[val.value].classList.add('active');
+            }   
+          })
+        })
+    }
+
+    if(!(obj.rate === "") && obj.type === 'pain'){
+      this.painBtnType.forEach(val =>{
+           if(val.type === obj.pain){
+               this.painRating(index, val.value);
+           }
+      })
+    } else if(!(obj.rate === "") && obj.type === 'polyuria'){
+         this.painBtnType.forEach(val =>{
+           if(val.type === obj.pain){
+               this.polyuriaRating(index, val.value);
+          }   
+         })
+    }
+})
+}
+
+
+painType(index, value, no){
+        if(this.complaintForms[index].pain.includes(value)){
+            var position = this.complaintForms[index].pain.indexOf(value);
+            this.complaintForms[index].pain.splice(position, 1);
+            document.getElementsByClassName('pain-'+ index)[no].classList.remove('active');
+        } else{
+          this.complaintForms[index].pain.push(value);
+          document.getElementsByClassName('pain-'+ index)[no].classList.add('active');
+        }   
+}
+
+
+    plyriaType(index, value, no){
+      if(this.complaintForms[index].pain.includes(value)){
+          var position = this.complaintForms[index].pain.indexOf(value);
+          this.complaintForms[index].pain.splice(position, 1);
+          document.getElementsByClassName('plyria-'+ index)[no].classList.remove('active');
+      }else{
+        this.complaintForms[index].pain.push(value);
+        document.getElementsByClassName('plyria-'+ index)[no].classList.add('active');
+      }
+  } 
+
+  painRating(index, no){
+    var value = parseInt(no) + 1
+    this.complaintForms[index].rate = value;
+    for (let i = 0; i < 10 ; i++) {
+      if (i != no) {
+        document.getElementsByClassName('painrating-'+ index)[i].classList.remove('active');
+      } else{
+        document.getElementsByClassName('painrating-'+ index)[i].classList.add('active');
+      }
+    }
+} 
+
+polyuriaRating(index, no){
+var value = parseInt(no) + 1
+this.complaintForms[index].rate = value;
+for (let i = 0; i < 10 ; i++) {
+  if (i != no) {
+    document.getElementsByClassName('rating-'+ index)[i].classList.remove('active');
+  } else{
+    document.getElementsByClassName('rating-'+ index)[i].classList.add('active');
+  }
+}
 }
 
 }
